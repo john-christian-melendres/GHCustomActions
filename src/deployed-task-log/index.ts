3b1452afeb33afc45afe7042ee4524304eeec280
john christian melendres <johnlmelendres@gmail.com>
2024-01-30 15:15:51 +0800

    add deployed task log

---
+++ src/deployed-task-log/index.ts
@@ -0,0 +1,54 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+
+export async function run(): Promise<void> {
+  try {
+    const startCommitHash = core.getInput('COMMIT_HASH', {trimWhitespace: true} )
+    const endCommitHash = 'HEAD'
+
+    console.log(startCommitHash)
+
+    let myOutput = '';
+    let myError = '';
+
+    const options = {
+      listeners: {
+      stdout: (data: Buffer) => {
+      myOutput += data.toString();
+      },
+      stderr: (data: Buffer) => {
+      myError += data.toString();
+      }
+      }
+    };
+
+    await exec.exec('git', ['log', '--pretty=format:', '{%n  \"commit\": \"%H\",%n  \"author\": \"%an\",%n  \"date\": \"%ad\",%n  \"message\": \"%f\"%n},' ,`${startCommitHash}..${endCommitHash}`],  options);
+    console.log(myOutput);
+    console.log(myError);
+
+    const test = `[${myOutput}]`
+    console.log(test)
+
+    core.setOutput('JS_VALUE', 'TEST VALUE')
+    // // get log rawText
+    // const logTxts = execSync(
+    //   `git log --pretty=format:"{%n  #'#commit#'#: #'#%H#'#,%n  #'#abbreviated_commit#'#: #'#%h#'#,%n  #'#tree#'#: #'#%T#'#,%n  #'#abbreviated_tree#'#: #'#%t#'#,%n  #'#parent#'#: #'#%P#'#,%n  #'#abbreviated_parent#'#: #'#%p#'#,%n  #'#refs#'#: #'#%D#'#,%n  #'#encoding#'#: #'#%e#'#,%n  #'#subject#'#: #'#%s#'#,%n  #'#sanitized_subject_line#'#: #'#%f#'#,%n  #'#body#'#: #'#%b#'#,%n  #'#commit_notes#'#: #'#%N#'#,%n  #'#verification_flag#'#: #'#%G?#'#,%n  #'#signer#'#: #'#%GS#'#,%n  #'#signer_key#'#: #'#%GK#'#,%n  #'#author#'#: {%n    #'#name#'#: #'#%aN#'#,%n    #'#email#'#: #'#%aE#'#,%n    #'#date#'#: #'#%aD#'#%n  },%n  #'#commiter#'#: {%n    #'#name#'#: #'#%cN#'#,%n    #'#email#'#: #'#%cE#'#,%n    #'#date#'#: #'#%cD#'#%n  }%n}," ${startCommitHash}..${endCommitHash}`,
+    //   { encoding: 'utf8' }
+    // ).toString()
+
+    // // transform json
+    // const logJson = `[${logTxts.slice(0, -1).replace(/"/g, "'").replace(/#'#/g, '"')}]`
+
+    // fs.writeFileSync(path.join(__dirname, 'log.json'), logJson)
+
+    // // parse json and do what you need
+    // const subjectLines = JSON.parse(logJson).map(
+    //   item => item['sanitized_subject_line']
+    // )
+    // console.info('subjectLines', subjectLines)
+
+    // core.setOutput('JSONTEST', subjectLines )
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
