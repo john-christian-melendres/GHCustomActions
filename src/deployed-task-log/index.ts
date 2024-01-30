92d6d4fa97df6ff80f5fd3600efdc387f97051e2
john christian melendres <johnlmelendres@gmail.com>
2024-01-30 18:40:12 +0800

    update script

--- src/deployed-task-log/index.ts
+++ src/deployed-task-log/index.ts
@@ -23,7 +23,7 @@ export async function run(): Promise<void> {
       }
     };
 
-    await exec.exec('git', ['log', '--pretty=format:', `{%n  \"commit\": \"%H\",%n  \"author\": \"%an\",%n  \"date\": \"%ad\",%n  \"message\": \"%f\"%n},` ,`${startCommitHash}^1..${endCommitHash}`],  options);
+    await exec.exec('git', ['log', `--pretty=format:'{%n  \"commit\": \"%H\",%n  \"author\": \"%an\",%n  \"date\": \"%ad\",%n  \"message\": \"%f\"%n},'` ,`${startCommitHash}^1..${endCommitHash}`],  options);
     console.log(myOutput);
     console.log(myError);
 
