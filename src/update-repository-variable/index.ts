37c64e96fd631d59b16163423cb715176725ab7b
john christian melendres <johnlmelendres@gmail.com>
2024-02-01 13:27:31 +0800

    add update variable

---
+++ src/update-repository-variable/index.ts
@@ -0,0 +1,42 @@
+import * as core from '@actions/core';
+import { Octokit } from '@octokit/rest'
+
+async function run(): Promise<void> {
+  try {
+    const token = core.getInput('token', {trimWhitespace: true, required: true} ) || '';
+    const repository = core.getInput('repository', {trimWhitespace: true, required: true} ) || '';
+    const prefix = core.getInput('prefix', {trimWhitespace: true} ) || '';
+    const newValue = core.getInput('new-value', {trimWhitespace: true, required: true} ) || '';
+    const env = core.getInput('environment', {trimWhitespace: true} ) || 'INTEGRATION';
+    const variableName = core.getInput('variable-name', {trimWhitespace: true} ) || '';
+
+    let repositoryVariable = `${ prefix.toLowerCase() }${ env.toUpperCase() }`;
+
+    if(variableName) {
+        repositoryVariable = variableName
+    }
+
+    const [owner, repo] = repository.split('/');
+
+    if(!token) return ;
+
+    const octokit = new Octokit({auth: token})
+
+    const test = await octokit.request(`PATCH /repos/${owner}/${repo}/actions/variables/${repositoryVariable}`, {
+        owner,
+        repo,
+        name: repositoryVariable,
+        value: newValue,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      })
+
+    console.log({test,env,newValue,variableName,prefix,repository })
+
+  } catch (error) {
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
+
+run()
