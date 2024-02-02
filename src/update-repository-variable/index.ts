842bf7e8011162a2f4a9e64d8339aaa445df2572
john christian melendres <johnlmelendres@gmail.com>
2024-02-02 15:18:18 +0800

    update

--- src/update-repository-variable/index.ts
+++ src/update-repository-variable/index.ts
@@ -1,60 +1,37 @@
-import * as core from '@actions/core';
+import * as core from '@actions/core'
 import { Octokit } from '@octokit/rest'
 
 async function run(): Promise<void> {
   try {
-    const token = core.getInput('token', {trimWhitespace: true, required: true} ) || '';
-    const repository = core.getInput('repository', {trimWhitespace: true, required: true} ) || '';
-    const prefix = core.getInput('prefix', {trimWhitespace: true} ) || '';
-    const newValue = core.getInput('new-value', {trimWhitespace: true, required: true} ) || '';
-    const env = core.getInput('environment', {trimWhitespace: true} ) || 'INTEGRATION';
-    const variableName = core.getInput('variable-name', {trimWhitespace: true} ) || '';
+    const token = core.getInput('token', { trimWhitespace: true, required: true }) || ''
+    const repository = core.getInput('repository', { trimWhitespace: true, required: true }) || ''
+    const newValue = core.getInput('new-value', { trimWhitespace: true, required: true }) || ''
+    const variableName =  core.getInput('variable-name', { trimWhitespace: true }) || ''
+    const env = core.getInput('environment', { trimWhitespace: true }) || 'INTEGRATION'
+    const prefix = core.getInput('prefix', { trimWhitespace: true }) || ''
 
-    let repositoryVariable = `${ prefix.toLowerCase() }${ env.toUpperCase() }`;
+    let repositoryVariable = `${prefix.toLowerCase()}${env.toUpperCase()}`
 
-    if(variableName) {
-        repositoryVariable = variableName
+    if (variableName) {
+      repositoryVariable = variableName
     }
 
-    console.log(prefix)
-    console.log(newValue)
-    console.log(env)
-    console.log(repositoryVariable)
-    console.log(variableName)
+    if (!token) return
 
-    const [owner, repo] = repository.split('/');
+    const octokit = new Octokit({ auth: token })
 
-    console.log(owner,repo )
-
-    // if(!token) return ;
-
-    const octokit = new Octokit({auth: token})
-
-    // const test = await octokit.request(`PATCH /repos/${owner}/${repo}/actions/variables/${repositoryVariable}`, {
-    //     owner,
-    //     repo,
-    //     name: repositoryVariable,
-    //     value: newValue,
-    //     headers: {
-    //       'X-GitHub-Api-Version': '2022-11-28'
-    //     }
-    //   })
-
-    const test = await octokit.request(`PATCH /repos/Zollner-Group/Profilservice/actions/variables/COMMIT_INTEGRATION`, {
-        name: 'COMMIT_INTEGRATION',
+    await octokit.request(
+      `PATCH /repos/${repository}/actions/variables/${repositoryVariable}`,
+      {
+        name: repositoryVariable,
         value: newValue,
         headers: {
           'X-GitHub-Api-Version': '2022-11-28',
           accept: 'application/vnd.github+json'
         }
-      })
-
-    console.log(test)
-    console.log({test,env,newValue,variableName,prefix,repository })
-    core.setOutput('json-value', {test,env,newValue,variableName,prefix,repository })
-
+      }
+    )
   } catch (error) {
-    console.log(error)
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
--- update-repository-variable/dist/index.js
+++ update-repository-variable/dist/index.js
@@ -28799,46 +28799,27 @@ async function run() {
     try {
         const token = core.getInput('token', { trimWhitespace: true, required: true }) || '';
         const repository = core.getInput('repository', { trimWhitespace: true, required: true }) || '';
-        const prefix = core.getInput('prefix', { trimWhitespace: true }) || '';
         const newValue = core.getInput('new-value', { trimWhitespace: true, required: true }) || '';
-        const env = core.getInput('environment', { trimWhitespace: true }) || 'INTEGRATION';
         const variableName = core.getInput('variable-name', { trimWhitespace: true }) || '';
+        const env = core.getInput('environment', { trimWhitespace: true }) || 'INTEGRATION';
+        const prefix = core.getInput('prefix', { trimWhitespace: true }) || '';
         let repositoryVariable = `${prefix.toLowerCase()}${env.toUpperCase()}`;
         if (variableName) {
             repositoryVariable = variableName;
         }
-        console.log(prefix);
-        console.log(newValue);
-        console.log(env);
-        console.log(repositoryVariable);
-        console.log(variableName);
-        const [owner, repo] = repository.split('/');
-        console.log(owner, repo);
-        // if(!token) return ;
+        if (!token)
+            return;
         const octokit = new rest_1.Octokit({ auth: token });
-        // const test = await octokit.request(`PATCH /repos/${owner}/${repo}/actions/variables/${repositoryVariable}`, {
-        //     owner,
-        //     repo,
-        //     name: repositoryVariable,
-        //     value: newValue,
-        //     headers: {
-        //       'X-GitHub-Api-Version': '2022-11-28'
-        //     }
-        //   })
-        const test = await octokit.request(`PATCH /repos/Zollner-Group/Profilservice/actions/variables/COMMIT_INTEGRATION`, {
-            name: 'COMMIT_INTEGRATION',
+        await octokit.request(`PATCH /repos/${repository}/actions/variables/${repositoryVariable}`, {
+            name: repositoryVariable,
             value: newValue,
             headers: {
                 'X-GitHub-Api-Version': '2022-11-28',
                 accept: 'application/vnd.github+json'
             }
         });
-        console.log(test);
-        console.log({ test, env, newValue, variableName, prefix, repository });
-        core.setOutput('json-value', { test, env, newValue, variableName, prefix, repository });
     }
     catch (error) {
-        console.log(error);
         if (error instanceof Error)
             core.setFailed(error.message);
     }
